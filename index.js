f7520eb7f66b060c35147e1105e0b48e1f751e3a
Xavier Jouppe <xajouppe@gmail.com>
2020-10-20 14:22:38 +0200

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const demarches = require("./demarches/index.js");
 const app = express();
-const port = 8000;
+const port = 3000;
 
 app.get("/", (req, res) => {
   res.json(demarches);
