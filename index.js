c150c9539cf6a52c54c7ef08374fdd828cbffd9f
Xavier Jouppe <xajouppe@gmail.com>
2020-10-20 16:10:08 +0200

    tech $PORT into account

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const demarches = require("./demarches/index.js");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8000;
 
 const notFound = (msg, request, response) => {
   console.log(`[404] ${request.path}`);
